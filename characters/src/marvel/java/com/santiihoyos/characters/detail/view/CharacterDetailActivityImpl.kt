2151ddbbd26c6f65932b8aaaace86d9e89533e21
Santi Hoyos <santiago.hoyos93@gmail.com>
2021-04-11 15:35:17 +0200

    ADD - layout for marvel character detail implemented

--- characters/src/marvel/java/com/santiihoyos/characters/detail/view/CharacterDetailActivityImpl.kt
+++ characters/src/marvel/java/com/santiihoyos/characters/detail/view/CharacterDetailActivityImpl.kt
@@ -7,11 +7,13 @@ import com.santiihoyos.characters.entity.Character
 class CharacterDetailActivityImpl: CharacterDetailActivity() {
 
     private val name by lazy { findViewById<AppCompatTextView>(R.id.characterDetailActivity_value_name) }
+    private val description by lazy { findViewById<AppCompatTextView>(R.id.characterDetailActivity_value_description) }
 
     override fun paintCharacterValues(character: Character) {
 
         setupToolbar(character.name ?: "")
         name.text = character.name
+        description.text = character.description
         refreshFavButtonState(character.id.toString())
     }
 
